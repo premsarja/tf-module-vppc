688736a400e96247886af647145eb9ebc170b086
premsarja <premsagar@prodot.in>
2024-03-10 17:49:47 +0530

    done

--- vpc.tf
+++ vpc.tf
@@ -2,6 +2,9 @@ resource "aws_vpc" "main" {
   cidr_block                = var.VPC_CIDR
   enable_dns_hostnames      = true
   tags = {
-    Name = "roboshop-{var.ENV}-vpc"
+    Name = "roboshop-${var.ENV}-vpc"
   }
 } 
+
+
+
