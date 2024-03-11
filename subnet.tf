d34e147f11e04e406722e05efbdc13afe1b4c318
premsarja <premsagar@prodot.in>
2024-03-11 19:02:16 +0530

    done

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "public_subnet" {
-  count = length(var.PUBLIC_SUBNET_CIDR)  
+  count      = length(var.PUBLIC_SUBNET_CIDR)  
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
   availability_zone = element(var.AZ,count.index)
@@ -9,9 +9,9 @@ resource "aws_subnet" "public_subnet" {
 }
 
 resource "aws_subnet" "private_subnet" {
-  count = length(var.PRIVATE_SUBNET_CIDR)  
+  count      = length(var.PRIVATE_SUBNET_CIDR)  
   vpc_id     = aws_vpc.main.id
-  cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR, count.index)
   availability_zone = element(var.AZ,count.index)
   tags = {
     Name = "roboshop-${var.ENV}-private-subnet-${element(var.AZ,count.index)}"
