f9315db62ffcc9b83e9159016d02168d8010a3b4
premsarja <premsagar@prodot.in>
2024-03-11 14:41:36 +0530

    done

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,4 @@
-resource "aws_vpc" "PUBLIC_SUBNET" {
-  count     = length(var.PUBLIC_SUBNET)
+resource "aws_vpc" "main" {
   cidr_block                = var.VPC_CIDR
   enable_dns_hostnames      = true
   tags = {
