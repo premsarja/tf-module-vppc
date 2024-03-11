857ced6a4b37faf0563b942fd905fe55661aea48
premsarja <premsagar@prodot.in>
2024-03-11 18:32:10 +0530

    done

--- subnet.tf
+++ subnet.tf
@@ -7,3 +7,13 @@ resource "aws_subnet" "public_subnet" {
     Name = "roboshop-${var.ENV}-public-subnet-${element(var.AZ,count.index)}"
   }
 }
+
+resource "aws_subnet" "private_subnet" {
+  count = length(var.PRIVATE_SUBNET_CIDR)  
+  vpc_id     = aws_vpc.main.id
+  cidr_block = element(var.PRIVATE_SUBNET_CIDR,count.index)
+  availability_zone = element(var.AZ,count.index)
+  tags = {
+    Name = "roboshop-${var.ENV}-private-subnet-${element(var.AZ,count.index)}"
+  }
+}
