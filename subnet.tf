6fc20d19f364c83425dbb22a75be6563c981db5e
premsarja <premsagar@prodot.in>
2024-03-11 15:04:46 +0530

    done

--- subnet.tf
+++ subnet.tf
@@ -1,5 +1,5 @@
-resource "aws_subnet" "PUBLIC_SUBNET_CIDR" {
-  count = length(var.PUBLIC_SUBNET_CIDR.id)  
+resource "aws_subnet" "public_subnet" {
+  count = length(var.PUBLIC_SUBNET_CIDR)  
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
 
