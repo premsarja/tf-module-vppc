5c1df8b3b1ad60ae1a1d0ea79fe484805a23c731
premsarja <premsagar@prodot.in>
2024-03-11 18:27:06 +0530

    done

--- subnet.tf
+++ subnet.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "public_subnet" {
   count = length(var.PUBLIC_SUBNET_CIDR)  
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.PUBLIC_SUBNET_CIDR,count.index)
-  availability_zone = element(var.AZ)
+  availability_zone = element(var.AZ,count.index)
   tags = {
     Name = "roboshop-${var.ENV}-public-subnet"-"${element(var.AZ,count.index)}"
   }
