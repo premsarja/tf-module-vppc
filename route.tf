9c65d5477fbf76ccc4761bea12e17f39762d05bc
premsarja <premsagar@prodot.in>
2024-03-12 12:53:07 +0530

    done

--- route.tf
+++ route.tf
@@ -6,8 +6,8 @@ resource "aws_route_table" "public_rt" {
     gateway_id = aws_internet_gateway.igw.id
   }
   
-  route = {
-    cidr_block =var.DEFAULT_VPC_CIDR
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_internet_gateway.igw.id
   }
 
@@ -16,25 +16,24 @@ resource "aws_route_table" "public_rt" {
   }
 }
 
-#add subnet association to route table
+# Add subnet association to route table
 
 resource "aws_route_table_association" "public_subnet_rt" {
-  count = length(aws_subnet.public_subnet.*.id)
-  subnet_id      = element(aws_subnet.public_subnet.*.id,count.index)
+  count          = length(aws_subnet.public_subnet.*.id)
+  subnet_id      = element(aws_subnet.public_subnet.*.id, count.index)
   route_table_id = aws_route_table.public_rt.id
 }
-
 
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
-  route = {
-    cidr_block =var.DEFAULT_VPC_CIDR
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
     vpc_peering_connection_id = aws_internet_gateway.igw.id
   }
 
-  route  {
-    cidr_block ="0.0.0.0/0"
+  route {
+    cidr_block    = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
 
@@ -43,10 +42,10 @@ resource "aws_route_table" "private_rt" {
   }
 }
 
-#add subnet association to route table
+# Add subnet association to route table
 
 resource "aws_route_table_association" "private_subnet_rt" {
-  count = length(aws_subnet.private_subnet.*.id)
-  subnet_id      = element(aws_subnet.private_subnet.*.id,count.index)
+  count          = length(aws_subnet.private_subnet.*.id)
+  subnet_id      = element(aws_subnet.private_subnet.*.id, count.index)
   route_table_id = aws_route_table.private_rt.id
 }
