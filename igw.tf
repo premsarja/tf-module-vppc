d573fa2ee66610c749aa77ac131e6ada261796c5
premsarja <premsagar@prodot.in>
2024-03-12 12:47:14 +0530

    done

--- igw.tf
+++ igw.tf
@@ -5,3 +5,24 @@ resource "aws_internet_gateway" "igw" {
     Name = "roboshop-${var.ENV}-igw"
   }
 }
+
+#crest ELASTIC IP address which would used by the NAT ,Ensure EIP is provisioned first and then NAT
+
+resource "aws_eip" "ngw_ip" {
+   vpc = true  
+    
+    tags ={ 
+    Name =  "roboshop-${var.ENV}-ngw-ip"
+  }
+}
+
+#CREATS NAT GATEWAY and will attached to PUBLIC SUBNET
+
+resource "aws_nat_gateway" "ngw" {
+  allocation_id = aws_eip.ngw_ip
+  subnet_id = aws_subnet.public_subnet.*.id[0]
+
+  tags = {
+    Name = "gw-NAT"
+  }
+}
--- route.tf
+++ route.tf
@@ -7,7 +7,8 @@ resource "aws_route_table" "public_rt" {
   }
   
   route = {
-    cidr_block =var.DEFAULT_VPC
+    cidr_block =var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_internet_gateway.igw.id
   }
 
   tags = {
@@ -27,11 +28,15 @@ resource "aws_route_table_association" "public_subnet_rt" {
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
-  # route {
-  #   cidr_block = "0.0.0.0/0"
-  #   gateway_id = aws_internet_gateway.igw.id
-  # }
+  route = {
+    cidr_block =var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_internet_gateway.igw.id
+  }
 
+  route = {
+    cidr_block ="0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id
+  }
 
   tags = {
     Name = "roboshop-${var.ENV}-prvt-rt"
