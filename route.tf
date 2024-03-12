a83c72f2841aacf5288a6ea05c495c3ac2e812e2
premsarja <premsagar@prodot.in>
2024-03-12 14:07:46 +0530

    done

--- route.tf
+++ route.tf
@@ -29,7 +29,7 @@ resource "aws_route_table" "private_rt" {
 
   route {
     cidr_block                = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_internet_gateway.igw.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
   route {
