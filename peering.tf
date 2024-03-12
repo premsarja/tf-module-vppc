e63c60dacbea721ab482535fb7e4e874400583fd
premsarja <premsagar@prodot.in>
2024-03-12 14:04:30 +0530

    done

--- peering.tf
+++ peering.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc_peering_connection" "peer" {
   vpc_id = var.DEFAULT_VPC_ID
-  peer_vpc_id   = aws_vpc.main.id
+  peer_vpc_id   = aws_vpc.main.id  #this is valid if both the VPC's are in the same account
   auto_accept = true
 }
--- route.tf
+++ route.tf
@@ -8,7 +8,7 @@ resource "aws_route_table" "public_rt" {
   
   route {
     cidr_block                = var.DEFAULT_VPC_CIDR
-    vpc_peering_connection_id = aws_internet_gateway.igw.id
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
 
   tags = {
