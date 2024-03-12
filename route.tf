eb6367daf8cecc3c2c0eb0929d8c49f8d36e6610
premsarja <premsagar@prodot.in>
2024-03-12 11:05:02 +0530

    done

--- route.tf
+++ route.tf
@@ -19,3 +19,26 @@ resource "aws_route_table_association" "public_subnet_rt" {
   subnet_id      = element(aws_subnet.public_subnet.*.id,count.index)
   route_table_id = aws_route_table.public_rt.id
 }
+
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.igw.id
+  # }
+
+
+  tags = {
+    Name = "roboshop-${var.ENV}-prvt-rt"
+  }
+}
+
+#add subnet association to route table
+
+# resource "aws_route_table_association" "public_subnet_rt" {
+#   count = length(aws_subnet.public_subnet.*.id)
+#   subnet_id      = element(aws_subnet.public_subnet.*.id,count.index)
+#   route_table_id = aws_route_table.public_rt.id
+# }
