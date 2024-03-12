f16b871a7be949603edbd83fba0e28e540a12365
premsarja <premsagar@prodot.in>
2024-03-12 11:07:32 +0530

    done

--- route.tf
+++ route.tf
@@ -37,8 +37,8 @@ resource "aws_route_table" "private_rt" {
 
 #add subnet association to route table
 
-# resource "aws_route_table_association" "public_subnet_rt" {
-#   count = length(aws_subnet.public_subnet.*.id)
-#   subnet_id      = element(aws_subnet.public_subnet.*.id,count.index)
-#   route_table_id = aws_route_table.public_rt.id
-# }
+resource "aws_route_table_association" "private_subnet_rt" {
+  count = length(aws_subnet.PRIVATE_SUBNET_CIDR.*.id)
+  subnet_id      = element(aws_subnet.PRIVATE_SUBNET_CIDR.*.id,count.index)
+  route_table_id = aws_route_table.private_rt.id
+}
