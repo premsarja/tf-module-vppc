13f905c8f328162ed13cbbca5406d49cc1f5d7c2
premsarja <premsagar@prodot.in>
2024-03-12 12:52:11 +0530

    done

--- route.tf
+++ route.tf
@@ -33,7 +33,7 @@ resource "aws_route_table" "private_rt" {
     vpc_peering_connection_id = aws_internet_gateway.igw.id
   }
 
-  route = {
+  route  {
     cidr_block ="0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.ngw.id
   }
