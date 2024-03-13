c13f867c23f1993a27939c7ea604bd12ae19c919
premsarja <premsagar@prodot.in>
2024-03-13 12:32:20 +0530

    done

--- outputs.tf
+++ outputs.tf
@@ -14,6 +14,17 @@ output "PRIVATE_SUBNET_CIDR" {
   value = aws_subnet.private_subnet.*.cidr_block
 }
 
+
+output "PUBLIC_SUBNET_IDS" {
+  value = aws_subnet.public_subnet.*.id
+}
+
+
+output "PRIVATE_SUBNET_IDS" {
+  value = aws_subnet.private_subnet.*.id
+}
+
+
 output "DEFAULT_VPC_ID" {
   value = var.DEFAULT_VPC_ID
 }
